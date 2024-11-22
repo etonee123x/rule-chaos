d40b057af94a5997ca291fc0311a565158f6af27
eto-ne-e123x <etonee123x@gmail.com>
2024-11-23 00:58:38 +0400

    working on messages

--- client/src/views/ViewPlay.tsx
+++ client/src/views/ViewPlay.tsx
@@ -4,27 +4,24 @@ import { doesMessageHasType } from '@/helpers/doesMessageHasType';
 import { useEffect, type FC } from 'react';
 
 export const ViewPlay: FC = () => {
-  useEffect(
-    () =>
-      addHandler((message) => {
-        if (doesMessageHasType(message, MessageType.PlayerJoinedSession)) {
-          return console.log('Оппа, присоединился');
-        }
+  useEffect(() =>
+    addHandler((message) => {
+      if (doesMessageHasType(message, MessageType.PlayerJoinedSession)) {
+        return console.log('Оппа, присоединился');
+      }
 
-        if (doesMessageHasType(message, MessageType.PlayerLeftSession)) {
-          return console.log('Оппа, отсоединился');
-        }
+      if (doesMessageHasType(message, MessageType.PlayerLeftSession)) {
+        return console.log('Оппа, отсоединился');
+      }
 
-        if (doesMessageHasType(message, MessageType.SessionWasStarted)) {
-          return console.log('Оппа, сессия началась');
-        }
+      if (doesMessageHasType(message, MessageType.SessionWasStarted)) {
+        return console.log('Оппа, сессия началась');
+      }
 
-        if (doesMessageHasType(message, MessageType.NewActivePlayer)) {
-          return console.log('Оппа, новый ход');
-        }
-      }),
-
-    addHandler(MessageType.PlayerJoinedSession, (message) => {}),
+      if (doesMessageHasType(message, MessageType.NewActivePlayer)) {
+        return console.log('Оппа, новый ход');
+      }
+    }),
   );
 
   return <>Игра!</>;
