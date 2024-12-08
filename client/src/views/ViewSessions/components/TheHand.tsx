6c99f95a454d1fbbdcc5f6c62a79b4ad4c3d112b
Evgeniy Uvarov <etonee123x@gmail.com>
2024-12-08 19:32:47 +0400

    update

--- client/src/views/ViewSessions/components/TheHand.tsx
+++ client/src/views/ViewSessions/components/TheHand.tsx
@@ -1,23 +1,32 @@
 import type { Item } from '@/helpers/message';
-import { UI } from '@/helpers/ui';
 import classNames from 'classnames';
-import { type FC, type HTMLAttributes } from 'react';
+import { type FC, type HTMLAttributes, type WheelEventHandler } from 'react';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   items: Array<Item>;
 }
 
-export const TheHand: FC<Props> = (props) => (
-  <div className={classNames([props.className, 'py-2'])}>
-    <ul className={classNames(['flex gap-2 overflow-x-auto h-full', UI.NO_SCROLLBAR])}>
-      {props.items.map((item, index) => (
-        <li
-          key={index}
-          className="h-full aspect-square border shrink-0 flex justify-center items-center cursor-pointer select-none"
-        >
-          <div>{item.text}</div>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+export const TheHand: FC<Props> = (props) => {
+  const onWheel: WheelEventHandler<HTMLUListElement> = (event) => {
+    if (!event.deltaY) {
+      return;
+    }
+
+    event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+  };
+
+  return (
+    <div className={classNames([props.className])}>
+      <ul className={classNames('flex gap-2 py-2 overflow-x-auto h-full')} onWheel={onWheel}>
+        {props.items.slice(0, 12).map((item, index) => (
+          <li
+            key={index}
+            className="aspect-square max-h-24 border shrink-0 flex justify-center items-center cursor-pointer select-none"
+          >
+            <div>{item.text}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
